404114faf14b42b218c199e4ad1744acb9793250
Jfghanimah <41597521+Jfghanimah@users.noreply.github.com>
2019-10-12 19:09:30 -0500

    added memory->file csv writer

--- scripts/combine_datasets.py
+++ scripts/combine_datasets.py
@@ -1,8 +1,20 @@
 import csv
 
 
-
-# populate win_rate_dict
 with open("data/runs_augmented.csv", "r") as r:
     runs = csv.reader(r, delimiter=",")
     next(runs)
+
+    with open("data/races2.csv", "r") as r:
+        races = csv.reader(r, delimiter=",")
+        next(races)
+
+        # Somehow stitch together the two datasets???
+
+
+quit() # do you dont actually create the file below during testing
+
+# dump memory back into new file
+with open("data/runs_and_races.csv", "w", newline='') as w:
+    writer = csv.writer(w, delimiter=",")
+    writer.writerows(lines)
