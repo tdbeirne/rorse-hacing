7549fbc9381024c4e8b6bf25f5093729e413ad34
Jfghanimah <41597521+Jfghanimah@users.noreply.github.com>
2019-10-12 21:11:17 -0500

    finished basic preprocessing

--- models/sample_model.py
+++ models/sample_model.py
@@ -1,29 +1,41 @@
-import tensorflow as tf
+import time
 import numpy as np
+import tensorflow as tf
 from tensorflow.keras.layers import Dense, BatchNormalization, Dropout
 from tensorflow.keras.models import Sequential
 
 # Need to implement data
 horse_runs = np.loadtxt(open("data/runs_augmented.csv", "r"),  delimiter=",", skiprows=1)
-# WARNING: if you try to re-train a model on re-shuffled data IT WILL MEMORIZE IT!!
-# np.random.shuffle(horse_runs) 
-print(horse_runs)
-quit()
 
-(x_train, y_train), (x_test, y_test) = horse_runs.load_data()
+# WARNING: if you try to re-train a model on re-shuffled data IT WILL MEMORIZE IT!!
+np.random.shuffle(horse_runs) 
+
+# Remove IDs and Place/Win
+x = np.delete(horse_runs,[0,1,2,3,10,11,24],1) # 1-3 are ids, 10&11 are labels, 24 is time to finish
+
+# 10 is the place order label
+# 11 is the win label
+delete_indicies = [x for x in range(0,len(horse_runs[0])) if x != 11]
+y = np.delete(horse_runs, delete_indicies ,1)
+
+train_size = 70000
+x_train = x[:train_size]
+x_test = x[train_size:]
+y_train = y[:train_size]
+y_test = y[train_size:]
+
 
 EPOCHS = 10
 BATCH_SIZE = 128
-
 DENSE_LAYER_SIZE = 256
-DROPOUT_RATE = 0.4
+DROPOUT_RATE = 0.3
 
 NAME = f"{DENSE_LAYER_SIZE}-dense-{DROPOUT_RATE}-drop-{int(time.time())}"
 print(NAME)
 
 model = Sequential()
 
-model.add(Dense(DENSE_LAYER_SIZE/2, activation='relu',input_layer=())) #input layer
+model.add(Dense(DENSE_LAYER_SIZE//2, activation='relu', input_shape=(20,) )) #input layer shape
 model.add(BatchNormalization())
 model.add(Dropout(DROPOUT_RATE))
 
@@ -31,25 +43,23 @@ model.add(Dense(DENSE_LAYER_SIZE, activation='relu'))
 model.add(BatchNormalization())
 model.add(Dropout(DROPOUT_RATE))
 
-model.add(Dense(128, activation='relu'))
+model.add(Dense(DENSE_LAYER_SIZE//2, activation='relu'))
 model.add(BatchNormalization())
 model.add(Dropout(DROPOUT_RATE))
 
-model.add(Dense(10, activation='softmax'))
+model.add(Dense(1, activation='softmax'))
 
-tensorboard = TensorBoard(log_dir=f"tensorboard_logs\{NAME}")
+#tensorboard = TensorBoard(log_dir=f"tensorboard_logs\{NAME}")
+#opt = tf.keras.optimizers.adam(lr=1e-3, decay=1e-6)
 
-opt = tf.keras.optimizers.adam(lr=1e-3, decay=1e-6)
-
-model.compile(optimizer=opt,
+model.compile(optimizer="adam",
             loss='binary_crossentropy',
             metrics=['accuracy'])
 
-model.fit(x_train, y_train, batch_size=BATCH_SIZE, verbose=2,
+model.fit(x_train, y_train, batch_size=BATCH_SIZE, verbose=1,
         epochs=EPOCHS, validation_data=(x_test, y_test))
 
-
-model.evaluate(x_test, y_test)
+model.evaluate(x_test, y_test, verbose=2)
 
 input("Would you like to save the model? (Press ANY key too save.)")
 
